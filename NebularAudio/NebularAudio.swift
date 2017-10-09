b47b9153dea3ac6eda840081d8abb0b286cef47e
Daniel Kallin <daniel.kallin@gmail.com>
2017-10-09 15:35:03 +0200

    moved threading outside the framework, fade in/out

--- NebularAudio/NebularAudio.swift
+++ NebularAudio/NebularAudio.swift
@@ -1,74 +1,68 @@
-//
-//  NebularAudio.swift
-//  NebularAudio
-//
-//  Created by Linus Schwalbe on 2017-10-08.
-//  Copyright © 2017 skanaar. All rights reserved.
-//
-
 import AVFoundation
 
-private var musicPool: [AVAudioPlayer] = []
-private var effectPool: [AVAudioPlayer] = []
+fileprivate func sign(_ x: Float) -> Float {
+    if x < 0 { return -1 }
+    if x > 0 { return 1 }
+    return 0
+}
+
+class FadingPlayer {
+    let player: AVAudioPlayer
+    let speed: Float = 0.01
+    var targetVolume: Float = 0
+
+    init(_ player: AVAudioPlayer) {
+        self.player = player
+    }
+
+    func update(dt: Float) {
+        let step = speed * Float(dt)
+        if abs(player.volume - targetVolume) < 1.5*step {
+            player.volume = targetVolume
+        } else {
+            player.volume -= step * sgn(player.volume - targetVolume)
+        }
+        if player.isPlaying && player.volume == 0 {
+            player.stop()
+        }
+        if !player.isPlaying && player.volume > 0 {
+            player.play()
+        }
+    }
+}
 
 public class NebularAudio {
-    public init () {}
-    
-    public func playMusic (file: String) {
-        let url = Bundle.main.url(forResource: file, withExtension: "mp3")
-    
-        if (url != nil) {
-            let current = musicPool.filter { (player) -> Bool in
-                return player.isPlaying
-            }
-            
-            if let currentPlayer = current.first {
-                self.crossFade(player: currentPlayer, from: 1.0, to: 0.0)
-            }
-            
-            let audioPlayer = self.requestMusicPlayer(url: url!)
-    
-            audioPlayer.numberOfLoops = -1
-            audioPlayer.prepareToPlay()
-            audioPlayer.play()
+
+    private var musicPool = [URL: FadingPlayer]()
+
+    public func update(dt: Double) {
+        for e in musicPool {
+            e.update(dt: Float(dt))
         }
     }
     
-    public func playEffect (file: String, time: Int) {
-    
+    public func playMusic(file: String) {
+        guard let url = Bundle.main.url(forResource: file, withExtension: "mp3") else {
+            return
+        }
+        for e in musicPool {
+            e.targetVolume = 0
+        }
+        let player = startPlaying(url: url!)
+        player.targetVolume = 1
     }
     
-    private func crossFade (player: AVAudioPlayer, from: Float, to: Float) {
-        let fade: Int = Int(2.0) * 100
-    
-        for step in 0...fade {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(Float(step) * 0.01)) {
-                player.volume = from + (to - from) * (Float(step) / Float(fade))
-                
-                if (player.volume <= 0.0) {
-                    player.pause()
-                }
-            }
-        }
-    }
-    
-    private func requestMusicPlayer (url: URL) -> AVAudioPlayer {
-        let inPool = musicPool.filter { (player) -> Bool in
-            return player.isPlaying == false && player.url == url
-        }
-        
-        if let player = inPool.first {
+    private func buildPlayer(url: URL) -> FadingPlayer? {
+        if let player = musicPool[url] {
             return player
         }
-        
-        let player = try? AVAudioPlayer(contentsOf: url)
-        
-        musicPool.append(player!)
-        
-        return musicPool.last!
-    }
-    
-    private func requestEffectPlayer (url: URL) {
-    
+        if let audioPlayer = try? AVAudioPlayer(contentsOf: url) {
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.volume = 0
+            let player = FadingPlayer(audioPlayer)
+            musicPool[url] = player
+            return player
+        }
+        return nil
     }
 }
